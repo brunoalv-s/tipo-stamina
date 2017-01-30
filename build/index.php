b72886e6ae43021e4447587bf53065d0c74a124d
Bruno Alves <brunoalv.s@outlook.com>
2017-01-30 14:58:45 -0200

    Template Stamina usando bootstrap e sass, quase concluído

---
+++ build/index.php
@@ -0,0 +1,130 @@
+<?php
+    $title = 'Home';
+    $description = '';
+    $keywords = '';
+
+    include('includes/geral.php');
+    include('includes/head.php');
+?>
+
+<body>
+
+    <header class="navbar">
+        <nav class="navbar nav-top">
+            <div class="container">
+                <p>Email: <a href="mailto:email@dominio.com.br">email@dominio.com.br</a></p>
+                <p>Telefone: (11) 8888-8888</p>
+            </div>
+        </nav>
+        <nav class="navbar container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand">
+                    <img src="assets/images/logo.png" alt="">
+                </a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Galeria</a></li>
+                    <li><a href="#">Trainer</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section id="parallaxBar" class="col-md-12 banner-cover text-center" data-speed="2" data-type="background">
+        <header>
+            <h1>Olá, este é o <span>Stamina</span></h1>
+            <p>Um template responsivo em HTML5 totalmente desenvolvido pela <a href="<?=$author;?>"><?=$creditos;?></a></p>
+            <a href="#" class="btn btn-cta">Assista nosso Vídeo</a>
+        </header>
+    </section>
+
+    <div class="services">
+        <div class="container">
+            <div class="col-md-4">
+                <div class="services-wrapper">
+                    <img src="assets/images/dumbbell.svg" alt="">
+                    <h3>Weight Lifting</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+                    <a href="#" class="btn btn-cta-small">More <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="services-wrapper">
+                    <img src="assets/images/exercise.svg" alt="">
+                    <h3>Running</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <a href="#" class="btn btn-cta-small">More <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="services-wrapper">
+                    <img src="assets/images/yoga-carpet.svg" alt="">
+                    <h3>Yoga</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+                    <a href="#" class="btn btn-cta-small">More <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="about">
+        <div class="container">
+            <div class="about-text">
+                <h2>Fitness Expert</h2>
+                <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+
+            </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="box">
+                            <a href="#">
+                                <img src="assets/images/trainer-1.jpg" alt="">
+                                <div class="title">
+                                    <h3>
+                                        Name
+                                    </h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box">
+                            <a href="#">
+                                <img src="assets/images/trainer-2.jpg" alt="">
+                                <div class="title">
+                                    <h3>
+                                        Name
+                                    </h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box">
+                            <a href="#">
+                                <img src="assets/images/trainer-3.jpg" alt="">
+                                <div class="title">
+                                    <h3>
+                                        Name
+                                    </h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+
+<?php include('includes/footer.php'); ?>
